c4ffecc667073772c22700d41b9d215d22a2ab47
Mikakolla <sergezhukov16@gmail.com>
2022-10-22 13:10:18 +0300

    homework35 hibernate

---
+++ src/main/java/com/example/hibernate/config/DatabaseConfig.java
@@ -0,0 +1,46 @@
+package com.example.hibernate.config;
+
+import com.example.hibernate.entity.Car;
+import lombok.AllArgsConstructor;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+
+@org.springframework.context.annotation.Configuration
+public class DatabaseConfig {
+
+
+    @Value("${hibernate.connection.driver_class}")
+    private String driver_class;
+    @Value("${hibernate.connection.url}")
+    private String url;
+    @Value("${hibernate.connection.username}")
+    private String username;
+    @Value("${hibernate.connection.password}")
+    private String password;
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+
+    @Bean
+    Configuration configuration() {
+        Configuration configuration = new Configuration();
+        configuration.setProperty("hibernate.connection.driver_class", driver_class);
+        configuration.setProperty("hibernate.connection.url", url);
+        configuration.setProperty("hibernate.connection.username", username);
+        configuration.setProperty("hibernate.connection.password", password);
+        configuration.setProperty("hibernate.dialect", dialect);
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+        configuration.addAnnotatedClass(Car.class);
+        return configuration;
+    }
+
+    @Bean
+    SessionFactory factory(Configuration configuration) throws HibernateException {
+        return configuration.buildSessionFactory();
+    }
+}
---
+++ src/main/java/com/example/hibernate/entity/Brand.java
@@ -0,0 +1,5 @@
+package com.example.hibernate.entity;
+
+public enum Brand {
+    AUDI, BMW, VW, VOLVO, OPEL
+}
